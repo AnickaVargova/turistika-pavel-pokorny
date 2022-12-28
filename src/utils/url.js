f54631f3e97799b9df5bee052456ff21df276790
Anna Vargova <anna.vargova01@currys.co.uk>
2022-12-28 09:44:08 +0100

    url changed

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,5 @@
-export const apiUrl = 'http://localhost:8080';
-// export const apiUrl = 'https://hrobecky.ddns.net';
+// export const apiUrl = 'http://localhost:8080';
+export const apiUrl = 'https://hrobecky.ddns.net';
 export const testUrl = 'https://turistika-test.netlify.app/';
 
 //TODO add both endpoints
