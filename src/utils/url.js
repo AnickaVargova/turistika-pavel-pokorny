db97483042dc18f25184d4cf341dfa1e88e8c163
Anna Vargova <anna.vargova01@currys.co.uk>
2022-12-29 10:28:30 +0100

    test url updated

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,4 @@
 // export const apiUrl = 'http://localhost:8080';
 export const apiUrl = 'https://hrobecky.ddns.net';
-export const testUrl = 'https://turistika-test.netlify.app/';
+export const testUrl = 'https://turistika-test.netlify.app';
+
