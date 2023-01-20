d9cf88f5a349a07108ec64215cb3ac0da137a0f0
Anna Vargova <anna.vargova01@currys.co.uk>
2023-01-20 19:03:34 +0100

    testing deploy from this branch

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,5 @@
 // export const apiUrl = 'http://localhost:8080';
+
 export const apiUrl = 'https://hrobecky.ddns.net';
 export const testUrl = 'https://turistika-test.netlify.app';
 // export const mapaUrl = 'http://localhost:8083/';
