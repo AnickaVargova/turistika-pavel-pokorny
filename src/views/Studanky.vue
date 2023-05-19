cde4cf742e5767c9d122757f38144f3404e41dc1
Anna <anna.vargova@bloomreach.com>
2023-05-19 15:42:48 +0200

    vysvetlivky k mape odstraneny ze studanek

--- src/views/Studanky.vue
+++ src/views/Studanky.vue
@@ -17,7 +17,7 @@ export default {
         uvodniText: [
           {
             textOdstavce:
-              "Od ledna 2023 začínám přidávat i hesla studánek brněnského okolí. Na rozdíl od pomníčků jsou studánky daleko častěji mezi lidmi předmětem zájmu, hledání a bádání. Nejdůkladnějším studánkovým webem jsou <span><a href='https://www.estudanky.eu/' target='_blank' class='vnitrniOdkaz'>E-studánky</a></span>.  Ale v posledních letech tento web sklouzává k zaznamenávání nefunkčních pump na návsích a kdejakého bahniska v lese. Ty tady nenajdete. Snaha je dávat sem studánky fungující. Bohužel v posledních  suchých letech slabší prameny ztrácejí vodu a tečou jen občas.<br>Studánky jsou zakresleny v mapě na úvodní straně <strong>fialovou</strong> kapkou. “Klasické” pomníčky jsou <strong>modré</strong>, sochy v přírodě <strong>zelené</strong>, sakrální objekty (kříže, boží muka, kapličky, křížové cesty) <strong>žluté</strong>. Smírčí kameny a trampské chaty nejsou v mapě zakresleny nijak, první z důvodů bezpečnosti, druhé kvůli ochraně soukromí.",
+              "Od ledna 2023 začínám přidávat i hesla studánek brněnského okolí. Na rozdíl od pomníčků jsou studánky daleko častěji mezi lidmi předmětem zájmu, hledání a bádání. Nejdůkladnějším studánkovým webem jsou <span><a href='https://www.estudanky.eu/' target='_blank' class='vnitrniOdkaz'>E-studánky</a></span>.  Ale v posledních letech tento web sklouzává k zaznamenávání nefunkčních pump na návsích a kdejakého bahniska v lese. Ty tady nenajdete. Snaha je dávat sem studánky fungující. Bohužel v posledních  suchých letech slabší prameny ztrácejí vodu a tečou jen občas."
           },
         ],
         kategoriePomnicky: [],
