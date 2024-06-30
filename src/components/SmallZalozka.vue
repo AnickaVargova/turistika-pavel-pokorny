32cb7230cf7953da3abdc0cf351a1567c81f5040
Anna <anna.vargova@bloomreach.com>
2024-06-30 15:54:13 +0200

    empty zalozka bug fixed

--- src/components/SmallZalozka.vue
+++ src/components/SmallZalozka.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class = 'zalozkaSmall' v-on:click="goToPar" v-if="zalozka.text">
-        {{zalozka.text}}
-    </div>
+  <div class="zalozkaSmall" v-on:click="goToPar" v-if="zalozka.text.length">
+    {{ zalozka.text }}
+  </div>
 </template>
 
 <script>
 export default {
   props: ["zalozka"],
- 
+
   methods: {
-    goToPar(){
-        sessionStorage.setItem("paragraphId", this.zalozka.paragraphId);
-    }
-  }
+    goToPar() {
+      sessionStorage.setItem("paragraphId", this.zalozka.paragraphId);
+    },
+  },
 };
 </script>
 
 <style>
 .zalozkaSmall {
-    border: 1px solid grey;
-    padding: 5px;
-    margin-right: 5px;
-    margin-bottom: 5px;
-    border-radius: 5px;
-    background-color: #cae0f0;
-    color: #2c3e50;
-    display:flex;
-    align-items: center;
+  border: 1px solid grey;
+  padding: 5px;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  border-radius: 5px;
+  background-color: #cae0f0;
+  color: #2c3e50;
+  display: flex;
+  align-items: center;
 }
 
 .zalozkaSmall:hover {
@@ -34,5 +34,4 @@ export default {
   color: black;
   background-color: #b7d8f0;
 }
-
 </style>
